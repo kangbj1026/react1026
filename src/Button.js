373311511770ab55387f715d1352604109e17322
kangbj1026 <kangbj1026@naver.com>
2022-07-14 21:02:49 +0900

    React

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+import styled from "./Button.module.css";
+function Button({text}) {
+	return <button className={styled.btn}>{text}</button>
+}
+Button.prototype = {
+	text: PropTypes.string.isRequired,
+
+}
+export default Button;
