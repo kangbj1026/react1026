12c03dc310dc44f2415bd0fc497f357e2384bc2b
kangbj1026 <kangbj1026@naver.com>
2022-07-17 16:35:05 +0900

    React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import Btn from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+	const [counter, setValue] = useState(0);
+	const [keyword, setKeyword] = useState("");
+	const onClick = () => setValue((def) => def + 1);
+	const onChange = (e) => setKeyword(e.target.value);
+	useEffect(()=>{
+		console.log("I run only once");
+	}, []);
+	useEffect(()=>{
+		console.log("I run when 'keyword' changes");
+	}, [keyword]);
+	useEffect(()=>{
+		console.log("I run when 'counter' changes");
+	}, [counter]);
+	useEffect(()=>{
+		console.log("I run when keyword & counter changes");
+	}, [keyword, counter]);
 	return (
 		<div>
-			<h1 className={styles.title}>Welcome Back!!!!!</h1>
-			<Btn text={"Continue"}/>
+			<input value={keyword} onChange={onChange} type={"text"} placeholder="Searh here..."/>
+			<h1>{counter}</h1>
+			<button onClick={onClick}>Click me</button>
 		</div>
 	);
 }
--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import styled from "./Button.module.css";
+
 function Button({text}) {
 	return <button className={styled.btn}>{text}</button>
 }
