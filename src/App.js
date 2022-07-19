43ad3346b3edf0ee33d4f7e7c39da3a07325b29f
kangbj1026 <kangbj1026@naver.com>
2022-07-19 09:33:56 +0900

    React

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ function Hi() {
 	return <h1>Hello</h1>
 }
 const Bye = () => <h1>Bye</h1>
-
 function App() {
 	const [showing, setShowing] = useState(true);
 	const onClick = () => {
 		setShowing((def) => !def);
 	}
+	const boy = document.body.style;
+	const Color = () => {
+	useEffect(()=>{
+		boy.backgroundColor = "#000000c2";
+		return () => boy.backgroundColor = "white";
+	},[])
+	}
 	return (
 		<div>
+			{showing ? <Color/> : null}
 			{showing ? <Hi/> : <Bye/>}
 			<button onClick={onClick}>{showing ? "Show" : "Hide" }</button>
 		</div>
