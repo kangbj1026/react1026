ab23413ffaf3ac9ff2070fe655f7f455e9357b68
kangbj1026 <kangbj1026@naver.com>
2022-07-19 19:30:39 +0900

    React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import { useState, useEffect } from "react";
 function App() {
-	const [toDo,setToDo] = useState("");
-	const [toDos,setToDos] = useState([]);
-	const onChange = (eve) => setToDo(eve.target.value)
-	const onSubmit = (eve) => {
-		eve.preventDefault();
-		if (toDo === "") {
-			console.log(toDo);
-		}
-		setToDos(currentArray => [toDo, ...currentArray]);
-		setToDo("");
+	const [singers, setSingers] = useState("");
+	const [singArray, setSingArray] = useState([]);
+	const onChange = (e) => {
+		setSingers(e.target.value)
 	}
-	console.log(toDos);
+	const onSubmit = (e) => {
+		e.preventDefault();
+		setSingArray(def => [singers.length == 1 ? singers : singers+", " , ...def])
+	}
 	return <div>
-		<h1>My YO Dos {toDos.length}</h1>
+		<h1>Names : {singArray}</h1>
 		<form onSubmit={onSubmit}>
-		<input type={"text"} onChange={onChange} placeholder={"Write your to do..."} value={toDo}/>
-		<button>Add To Do</button>
+			<input type={"text"} placeholder="Singer .." onChange={onChange} value={singers}/>
+			<button>Submit</button>
 		</form>
 	</div>;
 }
