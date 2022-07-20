362c0a2be930131cdc572fd7f080eefed85eee1e
kangbj1026 <kangbj1026@naver.com>
2022-07-20 20:26:04 +0900

    React

---
+++ src/Api.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+function Api() {
+	const [loading, setLoadIng] = useState(true);
+	const [coins, setCoins] = useState([]);
+	useEffect(()=>{
+		fetch("https://api.coinpaprika.com/v1/tickers")
+		.then((response) => response.json())
+		.then((json) => {
+			setCoins(json);
+			setLoadIng(false);
+		});
+	},[])
+	return <div>
+		<h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+		{loading ? <strong>Loading...</strong> : null}
+		<select>
+			{coins.map((coin,index) => <option key={index}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
+		</select>
+	</div>;
+}
+
+export default Api;
--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import { useState, useEffect } from "react";
 function App() {
-	const [loading, setLoadIng] = useState(true);
-	const [coins, setCoins] = useState([]);
+	const [loading,setLoading] = useState(true);
+	const [movies,setMovies] = useState([]);
+	const getMovies = async() => {
+		const response = await fetch("https://yts.mx/api/v2/list_movies.jsonminimum_rating=8.5&sort_by=year");
+		const json = await response.json();
+		setMovies(json.data.movies)
+		setLoading(false)
+	}
 	useEffect(()=>{
-		fetch("https://api.coinpaprika.com/v1/tickers")
-		.then((response) => response.json())
-		.then((json) => {
-			setCoins(json);
-			setLoadIng(false);
-		});
+		getMovies()
 	},[])
+	console.log(movies);
 	return <div>
-		<h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-		{loading ? <strong>Loading...</strong> : null}
-		<select>
-			{coins.map((coin,index) => <option key={index}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
-		</select>
+		{loading ?
+		(<h1>Loading...</h1>)
+		:
+		<div>
+			{movies.map((movie) =>
+			<div key={movie.id}>
+				<h2><a href={movie.url}>{movie.title}</a></h2>
+				<p>{movie.summary}</p>
+				<ul>
+					{movie.genres.map((g) => <li key={g}>{g}</li>)}
+				</ul>
+				<img src={movie.medium_cover_image} />
+			</div>
+			)}
+		</div>
+		}
 	</div>;
 }
 
